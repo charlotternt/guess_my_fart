9b7d00a26c76805bdc275255955b1925bec45bcd
charlotte <charlotte.renault@epitech.eu>
2024-01-24 21:00:06 +0100

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,47 @@
 <template>
   <div class="App">
     <h1>Guess my fart?</h1>
-    <button @click="initiateAudio">Play</button>
+    <button @touchstart="initiateAudio" @click="initiateAudio">Play</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      initiated: false,
+    };
+  },
   methods: {
     initiateAudio() {
-      const audioFiles = [
-        require('@/assets/audio01.mp3'),
-        require('@/assets/audio02.mp3'),
-        require('@/assets/audio03.mp3'),
-        require('@/assets/audio04.mp3')
-      ];
+      if (!this.initiated) {
+        const audioFiles = [
+          require('@/assets/audio01.mp3'),
+          require('@/assets/audio02.mp3'),
+          require('@/assets/audio03.mp3'),
+          require('@/assets/audio04.mp3')
+        ];
 
-      const randomAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
-      const audioPlayer = new Audio(randomAudio);
+        const randomAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+        const audioPlayer = new Audio(randomAudio);
 
-      audioPlayer.addEventListener('error', (e) => {
-        console.error('Erreur de lecture audio:', e);
-      });
+        audioPlayer.addEventListener('error', (e) => {
+          console.error('Erreur de lecture audio:', e);
+        });
 
-      audioPlayer.addEventListener('canplaythrough', () => {
-        audioPlayer.play()
-          .then(() => {
-            console.log('Lecture audio démarrée avec succès');
-          })
-          .catch((error) => {
-            console.error('Erreur de démarrage de la lecture audio:', error);
-          });
-      });
+        audioPlayer.addEventListener('canplaythrough', () => {
+          audioPlayer.play()
+            .then(() => {
+              console.log('Lecture audio démarrée avec succès');
+            })
+            .catch((error) => {
+              console.error('Erreur de démarrage de la lecture audio:', error);
+            });
+        });
+
+        this.initiated = true;
+      }
     }
   }
 }
